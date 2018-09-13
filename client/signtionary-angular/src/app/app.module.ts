56a4d04da7757fb89779d7bd8a96ce9e507c4e3e
Kevin Santacruz <defonix0@gmail.com>
2018-09-13 08:36:38 -0500

    Youtube parse url

--- client/signtionary-angular/src/app/app.module.ts
+++ client/signtionary-angular/src/app/app.module.ts
@@ -1,34 +1,9 @@
-/*import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { AngularFireModule } from 'angularfire2';
-import { AngularFirestoreModule } from 'angularfire2/firestore';
-import { environment } from '../environments/environment';
-
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    AngularFireModule.initializeApp(environment.firebase),
-    AngularFirestoreModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
-export class AppModule { }*/
-
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
-import { AppComponent } from './app.component';
-
+import { AppComponent, SafePipe } from './app.component';
 import { environment } from '../environments/environment';
+import { HttpModule } from '@angular/http';
 
 //AngularFireStuff
 import { AngularFireModule} from 'angularfire2';
@@ -36,7 +11,8 @@ import { AngularFirestore, AngularFirestoreModule} from 'angularfire2/firestore'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SafePipe
   ],
   imports: [
     BrowserModule,
